916056039cef6f5e58c9ce3f2edb2a6f5b6c3cae
YamiBeto <acdc0237@gmail.com>
2025-08-15 22:23:25 -0300

    Bras

--- src/app/components/lista-personagem/lista-personagem.ts
+++ src/app/components/lista-personagem/lista-personagem.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CardPersonagem } from '../card-personagem/card-personagem';
 import { PersonagensService } from '../../services/personagens-service';
 
@@ -11,18 +11,34 @@ interface IPersonagem {
 
 @Component({
   selector: 'app-lista-personagem',
-  imports: [CardPersonagem],
+  imports: [CardPersonagem,],
   templateUrl: './lista-personagem.html',
   styleUrl: './lista-personagem.css',
 })
-export class ListaPersonagem {
+export class ListaPersonagem implements OnInit, OnDestroy{
   personagens: IPersonagem[] = [];
 
-  constructor(private personagemService: PersonagensService) {
-    this.personagens = this.personagemService.getPersonagens();
+  constructor(private personagemService: PersonagensService) {}
+  
+
+  ngOnInit() {
+    console.log('OnInit')
+
+    // this.personagens = this.personagemService.getPersonagens();
+    this.personagemService.getPersonagens().subscribe(
+      personagens =>{
+        this.personagens = personagens;
+      }
+    )
   }
 
   incremetarVotoPersonagem(id: number) {
     this.personagemService.votarPersonagem(id);
+  }
+
+  ngOnDestroy(){
+    console.log('Destroi')
+    throw new Error('Method not implemented.');
+    
   }
 }
--- src/app/services/personagens-service.ts
+++ src/app/services/personagens-service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 interface IPersonagem {
   id: number;
@@ -10,48 +12,62 @@ interface IPersonagem {
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class PersonagensService {
+  constructor(
+    private httpCliente: HttpClient
+  ){ }
+  urlApi = 'http://localhost:3000/personagens'
   private personagens: IPersonagem[] = [
-    {
-      id: 113231,
-      nome: 'Rick Sanchez',
-      imagem: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-      votos: 0,
-    },
-    {
-      id: 12312,
-      nome: 'Rick Sanchez',
-      imagem: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-      votos: 0,
-    },
-    {
-      id: 3342342,
-      nome: 'Rick Sanchez',
-      imagem: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-      votos: 0,
-    },
-    {
-      id: 4234234,
-      nome: 'Rick Sanchez',
-      imagem: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
-      votos: 0,
-    },
-    {
-      id: 5234234,
-      nome: 'Rick Sanchez',
-      imagem: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
-      votos: 0,
-    },
+    // {
+    //   id: 113231,
+    //   nome: 'Rick Sanchez',
+    //   imagem: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    //   votos: 0,
+    // },
+    // {
+    //   id: 12312,
+    //   nome: 'Rick Sanchez',
+    //   imagem: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    //   votos: 0,
+    // },
+    // {
+    //   id: 3342342,
+    //   nome: 'Rick Sanchez',
+    //   imagem: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+    //   votos: 0,
+    // },
+    // {
+    //   id: 4234234,
+    //   nome: 'Rick Sanchez',
+    //   imagem: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+    //   votos: 0,
+    // },
+    // {
+    //   id: 5234234,
+    //   nome: 'Rick Sanchez',
+    //   imagem: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+    //   votos: 0,
+    // },
   ];
 
-  getPersonagens() {
-    return this.personagens;
+  getPersonagens(): Observable<any> {
+    return this.httpCliente.get(this.urlApi)
   }
 
   votarPersonagem(id: number) {
     const personagem = this.personagens.find(
-      (personagem) => personagem.id === id
+      () => personagem.id === id
     );
     personagem!.votos++;
   }
+
+  
+  // votarPersonagem(id: number) {
+  //   const personagem = this.personagens.find(
+  //     (personagem) => personagem.id === id
+  //   );
+  //   personagem!.votos++;
+  // }
 }
